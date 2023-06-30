77ff57ad62edf34f9453f89cad84ddb7d1a6d664
lahirujay99 <lahirujayawardana94@gmail.com>
2023-06-30 14:17:12 +0530

    if temRating? display it or display rating

--- src/StarRating.js
+++ src/StarRating.js
@@ -33,7 +33,7 @@ export default function StarRating({ maxStar = 5 }) {
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
