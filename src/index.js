83062a04b8a76f06f1b8a3df60e7b75c340df802
lahirujay99 <lahirujayawardana94@gmail.com>
2023-07-05 12:16:28 +0530

    fetching data using useEffect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App1";
+//import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxStar={9}
       message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
-    />
+    /> */}
   </React.StrictMode>
 );
